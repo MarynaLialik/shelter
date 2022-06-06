29dce106ce3b314744d0fc0cb6d49105e6781632
maniuska <marina.shatinskaya@gmail.com>
2022-06-07 01:08:48 +0200

    fix: add closing events for burger menu

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,10 +1,13 @@
 console.log("hello");
 
 const burger = document.querySelector('.burger');
-const blur_area = document.querySelector(.blur_back);
+const blur_area = document.querySelector('.blur_back');
+//const nav_link_list = document.querySelectorAll('navigation_main');
+const nav_link_list = document.getElementsByClassName('navigation__link');
+console.log(nav_link_list);
 
-burger.addEventListener('click', showMenu);
-function showMenu()
+burger.addEventListener('click', toggleMenu);
+function toggleMenu()
 {
     document.querySelector('.header__navigation').classList.toggle("active-main-mobile");
     document.querySelector('.navigation').classList.toggle("vertical-navigation");
@@ -14,12 +17,8 @@ function showMenu()
     burger.classList.toggle("burger-rotate");  
 }
 
-blur_area.addEventListener ('click', showMenu);
-function closeMenu () {
-    document.querySelector('.header__navigation').classList.toggle("active-main-mobile");
-    document.querySelector('.navigation').classList.toggle("vertical-navigation");
-    document.querySelector('.header__wrapper').classList.toggle("active-header");
-    document.querySelector('.blur_back').classList.toggle("blur-back-active");
-
-    burger.classList.toggle("burger-rotate");  
+blur_area.addEventListener ('click', toggleMenu);
+for (let i = 0; i < nav_link_list .length; i++) {
+    nav_link_list[i].addEventListener('click', toggleMenu);
 }
+nav_link_list[3].addEventListener('click', toggleMenu);
--- pages/pets/our_pets.js
+++ pages/pets/our_pets.js
@@ -1,14 +1,19 @@
-console.log("hello");
-
 const burger = document.querySelector('.burger');
-burger.addEventListener('click', showMenu);
-function showMenu()
+const blur_area = document.querySelector('.blur_back');
+const nav_link_list = document.getElementsByClassName('navigation__link');
+
+burger.addEventListener('click', toggleMenu);
+
+function toggleMenu()
 {
     document.querySelector('.header__navigation').classList.toggle("active-pets-mobile");
     document.querySelector('.navigation').classList.toggle("vertical-navigation");
     document.querySelector('.header__wrapper').classList.toggle("active-header");
     document.querySelector('.blur_back').classList.toggle("blur-back-active");
-
-    burger.classList.toggle("burger-rotate");
-    
+    burger.classList.toggle("burger-rotate");  
 }
+blur_area.addEventListener ('click', toggleMenu);
+for (let i = 0; i < nav_link_list .length; i++) {
+    nav_link_list[i].addEventListener('click', toggleMenu);
+}
+nav_link_list[3].addEventListener('click', toggleMenu);
